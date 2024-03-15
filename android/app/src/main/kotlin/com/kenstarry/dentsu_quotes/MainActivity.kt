33187c9a827c840d329cb7738707d800ffa52385
KenStarry <starrycodes@gmail.com>
2024-03-15 14:06:10 +0300

    Dentsu Quotes beginning

---
+++ android/app/src/main/kotlin/com/kenstarry/dentsu_quotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kenstarry.dentsu_quotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const purple = Color(0xff4A2860);

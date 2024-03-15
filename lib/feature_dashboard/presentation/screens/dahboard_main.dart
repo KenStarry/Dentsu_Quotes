e1acdcedbf79dac16a18db3d6ea1537c06db415c
KenStarry <starrycodes@gmail.com>
2024-03-15 16:24:23 +0300

    Dashboard Main Implementation

--- lib/feature_dashboard/presentation/screens/dahboard_main.dart
+++ lib/feature_dashboard/presentation/screens/dahboard_main.dart
@@ -1,3 +1,4 @@
+import 'package:dentsu_quotes/core/presentation/components/avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -16,15 +17,31 @@ class _DashboardMainState extends State<DashboardMain> {
         backgroundColor: Theme.of(context).primaryColor,
         elevation: 0,
         leadingWidth: 80,
+        toolbarHeight: 70,
         leading: UnconstrainedBox(
           child: SvgPicture.asset(
             'assets/images/logo.svg',
-            width: 40,
-            height: 40,
+            width: 35,
+            height: 35,
           ),
         ),
         actions: [
           //  profile picture
+          const Avatar(avatarUrl: '', size: Size(30, 30)),
+          const SizedBox(width: 8),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search_rounded,
+                  color: Theme.of(context).primaryColorDark)),
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'assets/images/menu.svg',
+                width: 24,
+                height: 24,
+                colorFilter: ColorFilter.mode(
+                    Theme.of(context).primaryColorDark, BlendMode.srcIn),
+              )),
         ],
       ),
     );

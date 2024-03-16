c7e39ef6e3527dd11fb4e0342851cad9dcbd4b2d
KenStarry <starrycodes@gmail.com>
2024-03-17 02:37:37 +0300

    Core use cases to dependencies

---
+++ lib/core/domain/use_cases/core_use_cases.dart
@@ -0,0 +1,7 @@
+import 'package:dentsu_quotes/core/domain/use_cases/listen_to_internet_status.dart';
+
+class CoreUseCases {
+  final ListenToInternetStatus listenToInternetStatus;
+
+  CoreUseCases({required this.listenToInternetStatus});
+}
--- lib/di/core_di.dart
+++ lib/di/core_di.dart
@@ -1,8 +1,14 @@
 import 'package:dentsu_quotes/core/data/repository/core_repository_impl.dart';
 import 'package:dentsu_quotes/core/domain/repository/core_repository.dart';
+import 'package:dentsu_quotes/core/domain/use_cases/core_use_cases.dart';
+import 'package:dentsu_quotes/core/domain/use_cases/listen_to_internet_status.dart';
 import 'package:get_it/get_it.dart';
 
 void coreDI({required GetIt locator}) {
   /// Core Repository
   locator.registerLazySingleton<CoreRepository>(() => CoreRepositoryImpl());
+
+  /// Core Use Cases
+  locator.registerLazySingleton<CoreUseCases>(
+      () => CoreUseCases(listenToInternetStatus: ListenToInternetStatus()));
 }

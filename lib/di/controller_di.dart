55431758651a8e62e3726974d615b240f284a769
KenStarry <starrycodes@gmail.com>
2024-03-17 18:25:56 +0300

    Added Auth DI implementation

--- lib/di/controller_di.dart
+++ lib/di/controller_di.dart
@@ -1,8 +1,10 @@
 import 'package:dentsu_quotes/core/presentation/controller/core_controller.dart';
+import 'package:dentsu_quotes/feature_auth/presentation/controller/auth_controller.dart';
 import 'package:dentsu_quotes/feature_dashboard/presentation/controller/dashboard_controller.dart';
 import 'package:get/get.dart';
 
 void invokeControllers() {
+  Get.lazyPut(() => AuthController(), fenix: true);
   Get.lazyPut(() => CoreController(), fenix: true);
   Get.lazyPut(() => DashboardController(), fenix: true);
 }
---
+++ lib/feature_auth/presentation/controller/auth_controller.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../../di/di.dart';
+import '../../domain/use_cases/auth_use_cases.dart';
+
+class AuthController extends GetxController {
+  final authUseCase = locator.get<AuthUseCases>();
+
+  late final StreamSubscription<AuthState> _subscription;
+  final currentEvent = AuthChangeEvent.signedOut.obs;
+  final currentSession = Rxn<Session>();
+
+  //  current screen
+  final isLogin = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    //  subscribe to auth
+    _subscription = authSubscription(onAuthStateChanged: (authState) {
+      currentEvent.value = authState.event;
+      currentSession.value = authState.session;
+    });
+  }
+
+  @override
+  void onClose() {
+    _subscription.cancel();
+    super.onClose();
+  }
+
+  void setIsLogin({required bool isLogin}) => this.isLogin.value = isLogin;
+
+  /// Sign Up
+  Future<void> signUp(
+          {required String email, required String password}) async =>
+      await authUseCase.signUp.call(email: email, password: password);
+
+  /// Sign In
+  Future<void> signIn(
+          {required String email, required String password}) async =>
+      await authUseCase.signIn.call(email: email, password: password);
+
+  /// Sign Out
+  Future<void> signOut() async => await authUseCase.signOut();
+
+  /// Get Auth User
+  User? getAuthUser() => authUseCase.getAuthUser();
+
+  /// Auth Subscription
+  StreamSubscription<AuthState> authSubscription(
+          {required Function(AuthState) onAuthStateChanged}) =>
+      authUseCase.authSubscription.call(onAuthStateChanged: onAuthStateChanged);
+}

bda0e44d54f71920088e25a458b37afd1c21a972
KenStarry <starrycodes@gmail.com>
2024-03-17 04:42:20 +0300

    View Quote Page code updates

--- lib/feature_quotes/presentation/screens/view_quote_page.dart
+++ lib/feature_quotes/presentation/screens/view_quote_page.dart
@@ -55,16 +55,12 @@ class _ViewQuotePageState extends State<ViewQuotePage>
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: Theme
-            .of(context)
-            .scaffoldBackgroundColor,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         body: Container(
           width: double.infinity,
           height: double.infinity,
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          color: Theme
-              .of(context)
-              .scaffoldBackgroundColor,
+          color: Theme.of(context).scaffoldBackgroundColor,
           child: CustomScrollView(
             physics: const BouncingScrollPhysics(),
             slivers: [
@@ -72,65 +68,47 @@ class _ViewQuotePageState extends State<ViewQuotePage>
               //  title
               SliverToBoxAdapter(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomBackBreadcrumb(
-                          backText: 'Back to all quotes',
-                          onBackPressed: () {
-                            _dashboardController.setViewQuoteActive(
-                                active: false);
-                          }),
-                      const SizedBox(height: 8),
-                      Text(widget.isNewQuote ? 'New Quote' : 'View Quote',
-                          style: TextStyle(
-                              fontSize:
-                              Theme
-                                  .of(context)
-                                  .textTheme
-                                  .titleMedium!
-                                  .fontSize,
-                              fontWeight: FontWeight.w600,
-                              color:
-                              Theme
-                                  .of(context)
-                                  .textTheme
-                                  .titleLarge!
-                                  .color)),
-                      const SizedBox(height: 8),
-                      TabBar(
-                          isScrollable: true,
-                          physics: widget.isNewQuote
-                              ? const NeverScrollableScrollPhysics()
-                              : const BouncingScrollPhysics(),
-                          indicatorSize: TabBarIndicatorSize.label,
-                          tabAlignment: TabAlignment.start,
-                          padding: EdgeInsets.zero,
-                          indicatorColor: Theme
-                              .of(context)
-                              .primaryColorLight,
-                          labelColor: Theme
-                              .of(context)
-                              .primaryColorLight,
-                          labelStyle: TextStyle(
-                              fontSize:
-                              Theme
-                                  .of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .fontSize,
-                              fontWeight: Theme
-                                  .of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .fontWeight),
-                          labelPadding: const EdgeInsets.only(right: 35),
-                          tabs: const [
-                            Tab(text: 'Quote Information'),
-                            Tab(text: 'Setup'),
-                            Tab(text: 'Benefits'),
-                          ])
-                    ],
-                  )),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomBackBreadcrumb(
+                      backText: 'Back to all quotes',
+                      onBackPressed: () {
+                        _dashboardController.setViewQuoteActive(active: false);
+                      }),
+                  const SizedBox(height: 8),
+                  Text(widget.isNewQuote ? 'New Quote' : 'View Quote',
+                      style: TextStyle(
+                          fontSize:
+                              Theme.of(context).textTheme.titleMedium!.fontSize,
+                          fontWeight: FontWeight.w600,
+                          color:
+                              Theme.of(context).textTheme.titleLarge!.color)),
+                  const SizedBox(height: 8),
+                  TabBar(
+                      isScrollable: true,
+                      physics: widget.isNewQuote
+                          ? const NeverScrollableScrollPhysics()
+                          : const BouncingScrollPhysics(),
+                      indicatorSize: TabBarIndicatorSize.label,
+                      tabAlignment: TabAlignment.start,
+                      padding: EdgeInsets.zero,
+                      indicatorColor: Theme.of(context).primaryColorLight,
+                      labelColor: Theme.of(context).primaryColorLight,
+                      labelStyle: TextStyle(
+                          fontSize:
+                              Theme.of(context).textTheme.bodyMedium!.fontSize,
+                          fontWeight: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .fontWeight),
+                      labelPadding: const EdgeInsets.only(right: 35),
+                      tabs: const [
+                        Tab(text: 'Quote Information'),
+                        Tab(text: 'Setup'),
+                        Tab(text: 'Benefits'),
+                      ])
+                ],
+              )),
 
               const SliverToBoxAdapter(child: SizedBox(height: 32)),
 
@@ -139,21 +117,27 @@ class _ViewQuotePageState extends State<ViewQuotePage>
                 child: SizedBox(
                   width: double.infinity,
                   height: 135 * 9,
-                  child: TabBarView(children: [
-                    ViewQuoteInformation(controllers: <TextEditingController>[
-                      _firstNameController,
-                      _middleNameController,
-                      _lastNameController,
-                      _leadSourceController,
-                      _quoteIdController,
-                      _businessUnitController,
-                      _leadIdController,
-                      _sourceController,
-                      _capturingUserController
-                    ], isNewQuote: widget.isNewQuote),
-                    ViewQuoteSetup(isNewQuote: widget.isNewQuote),
-                    ViewQuoteBenefits(isNewQuote: widget.isNewQuote),
-                  ]),
+                  child: TabBarView(
+                      physics: widget.isNewQuote
+                          ? const NeverScrollableScrollPhysics()
+                          : const BouncingScrollPhysics(),
+                      children: [
+                        ViewQuoteInformation(
+                            controllers: <TextEditingController>[
+                              _firstNameController,
+                              _middleNameController,
+                              _lastNameController,
+                              _leadSourceController,
+                              _quoteIdController,
+                              _businessUnitController,
+                              _leadIdController,
+                              _sourceController,
+                              _capturingUserController
+                            ],
+                            isNewQuote: widget.isNewQuote),
+                        ViewQuoteSetup(isNewQuote: widget.isNewQuote),
+                        ViewQuoteBenefits(isNewQuote: widget.isNewQuote),
+                      ]),
                 ),
               )
             ],

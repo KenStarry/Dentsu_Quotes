ba8ad2ab0ad83e1749a1f4537349edbe120d60e3
KenStarry <starrycodes@gmail.com>
2024-03-17 18:18:41 +0300

    Auth Repository implementation

---
+++ lib/feature_auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../../di/di.dart';
+import '../../domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final supabase = locator.get<SupabaseClient>();
+
+  /// Sign Up
+  @override
+  Future<void> signUp({required String email, required String password}) async {
+    try {
+      await supabase.auth.signUp(email: email, password: password);
+    } on SupabaseRealtimeError catch (error) {
+      throw Exception(error.message);
+    }
+  }
+
+  /// Sign In
+  @override
+  Future<void> signIn({required String email, required String password}) async {
+    try {
+      await supabase.auth.signInWithPassword(email: email, password: password);
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  /// Sign Out
+  @override
+  Future<void> signOut() async {
+    try {
+      await supabase.auth.signOut(scope: SignOutScope.local);
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  /// Auth User
+  @override
+  User? getAuthUser() => supabase.auth.currentUser;
+
+  /// Auth Subscription
+  @override
+  StreamSubscription<AuthState> authSubscription(
+          {required Function(AuthState) onAuthStateChanged}) =>
+      supabase.auth.onAuthStateChange.listen(onAuthStateChanged);
+}

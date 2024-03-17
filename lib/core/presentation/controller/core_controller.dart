5e79b7245e9e7beb1338d1554a6cc6523d55b139
KenStarry <starrycodes@gmail.com>
2024-03-17 05:13:03 +0300

    New Quote implementation in controller

--- lib/core/presentation/controller/core_controller.dart
+++ lib/core/presentation/controller/core_controller.dart
@@ -1,3 +1,4 @@
+import 'package:dentsu_quotes/core/domain/model/quote.dart';
 import 'package:get/get.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
@@ -6,6 +7,8 @@ import '../../domain/use_cases/core_use_cases.dart';
 
 class CoreController extends GetxController {
   final useCase = locator.get<CoreUseCases>();
+
+  final newQuote = Quote().obs;
 
   final hasInternet = false.obs;
 
@@ -17,10 +20,19 @@ class CoreController extends GetxController {
     listenToInternetStatus(onStatusChanged: (internetStatus) {
       hasInternet.value = internetStatus == InternetConnectionStatus.connected;
     });
+
+    ever(newQuote, (quote) {
+      print('--------------------${quote.toString()}');
+    });
   }
 
   void listenToInternetStatus(
           {required Function(InternetConnectionStatus status)
               onStatusChanged}) =>
       useCase.listenToInternetStatus.call(onStatusChanged: onStatusChanged);
+
+  void updateQuoteValue({required Quote updatedQuote}) {
+    newQuote.value = updatedQuote;
+    newQuote.refresh();
+  }
 }

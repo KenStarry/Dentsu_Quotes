62ecd17424cef53f23c47cbfb2d19ac57d547040
KenStarry <starrycodes@gmail.com>
2024-03-17 06:22:48 +0300

    FL Chart implementation

--- lib/feature_home/presentation/components/leads_content.dart
+++ lib/feature_home/presentation/components/leads_content.dart
@@ -1,4 +1,5 @@
 import 'package:dentsu_quotes/theme/colors.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
@@ -10,89 +11,54 @@ class LeadsContent extends StatefulWidget {
 }
 
 class _LeadsContentState extends State<LeadsContent> {
-  late final List<ChartData> chartData;
-
-  @override
-  void initState() {
-    super.initState();
-
-    chartData = <ChartData>[
-      ChartData(
-          category: 'Accounts',
-          y1: 0,
-          y2: 100,
-          y3: 75,
-          y4: 65,
-          y5: 43,
-          color: accent),
-      ChartData(
-          category: 'Credit',
-          y1: 0,
-          y2: 87,
-          y3: 54,
-          y4: 12,
-          y5: 55,
-          color: myBlue),
-      ChartData(
-          category: 'Insurance',
-          y1: 0,
-          y2: 66,
-          y3: 32,
-          y4: 65,
-          y5: 33,
-          color: myGreen),
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: double.infinity,
       color: Colors.transparent,
-      child: SfCartesianChart(
-        primaryXAxis: CategoryAxis(),
-        series: <CartesianSeries>[
-          StackedLineSeries<ChartData, String>(
-            dataSource: chartData,
-            pointColorMapper: (chartData, _) => chartData.color,
-            xValueMapper: (chartData, _) => chartData.category,
-            yValueMapper: (chartData, _) => chartData.y1,
-          ),
-          StackedLineSeries<ChartData, String>(
-            dataSource: chartData,
-            pointColorMapper: (chartData, _) => chartData.color,
-            xValueMapper: (chartData, _) => chartData.category,
-            yValueMapper: (chartData, _) => chartData.y2,
-          ),
-          StackedLineSeries<ChartData, String>(
-            dataSource: chartData,
-            pointColorMapper: (chartData, _) => chartData.color,
-            xValueMapper: (chartData, _) => chartData.category,
-            yValueMapper: (chartData, _) => chartData.y3,
-          ),
-
-        ],
-      ),
+      child: LineChart(LineChartData(
+          gridData: const FlGridData(show: false),
+          borderData: FlBorderData(show: false),
+          titlesData: const FlTitlesData(show: false),
+          lineBarsData: [
+            LineChartBarData(
+                show: true,
+                preventCurveOverShooting: true,
+                dotData: const FlDotData(show: false),
+                color: accentLight,
+                spots: const [
+                  FlSpot(0, 0),
+                  FlSpot(5, 15),
+                  FlSpot(8, 25),
+                  FlSpot(17, 13),
+                  FlSpot(22, 45)
+                ]),
+            LineChartBarData(
+                show: true,
+                preventCurveOverShooting: true,
+                dotData: const FlDotData(show: false),
+                color: myBlue,
+                spots: const [
+                  FlSpot(0, 0),
+                  FlSpot(5, 23),
+                  FlSpot(16, 8),
+                  FlSpot(20, 33),
+                  FlSpot(30, 20)
+                ]),
+            LineChartBarData(
+                show: true,
+                preventCurveOverShooting: true,
+                dotData: const FlDotData(show: false),
+                color: myGreen,
+                spots: const [
+                  FlSpot(0, 0),
+                  FlSpot(3, 9),
+                  FlSpot(7, 16),
+                  FlSpot(17, 32),
+                  FlSpot(24, 40)
+                ]),
+          ])),
     );
   }
-}
-
-class ChartData {
-  final String category;
-  final int y1;
-  final int y2;
-  final int y3;
-  final int y4;
-  final int y5;
-  final Color? color;
-
-  ChartData(
-      {required this.y1,
-      required this.y2,
-      required this.y3,
-      required this.y4,
-      required this.y5,
-      required this.category,
-      this.color});
 }

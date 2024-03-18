b69751cbf3867ed6394ce87bb34212a223584e49
KenStarry <starrycodes@gmail.com>
2024-03-18 13:40:35 +0300

    Zoom Drawer implementation

---
+++ lib/feature_dashboard/presentation/components/menu_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Theme.of(context).primaryColor,
+    );
+  }
+}
--- lib/feature_dashboard/presentation/screens/dahboard_main.dart
+++ lib/feature_dashboard/presentation/screens/dahboard_main.dart
@@ -2,6 +2,7 @@ import 'package:dentsu_quotes/core/presentation/components/avatar.dart';
 import 'package:dentsu_quotes/core/presentation/controller/core_controller.dart';
 import 'package:dentsu_quotes/feature_auth/presentation/controller/auth_controller.dart';
 import 'package:dentsu_quotes/feature_dashboard/presentation/components/bottom_bar_item.dart';
+import 'package:dentsu_quotes/feature_dashboard/presentation/components/menu_screen.dart';
 import 'package:dentsu_quotes/feature_dashboard/presentation/controller/dashboard_controller.dart';
 import 'package:dentsu_quotes/feature_leads/presentation/screens/leads_page.dart';
 import 'package:dentsu_quotes/feature_leads/presentation/screens/view_lead_page.dart';
@@ -11,6 +12,7 @@ import 'package:dentsu_quotes/feature_quotes/presentation/screens/view_quote_pag
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:get/get.dart';
 
 import '../../../feature_home/presentation/screens/home_page.dart';
@@ -26,6 +28,7 @@ class _DashboardMainState extends State<DashboardMain> {
   late final CoreController _coreController;
   late final AuthController _authController;
   late final DashboardController _dashboardController;
+  late final ZoomDrawerController _zoomDrawerController;
 
   @override
   void initState() {
@@ -34,6 +37,7 @@ class _DashboardMainState extends State<DashboardMain> {
     _coreController = Get.find<CoreController>();
     _authController = Get.find<AuthController>();
     _dashboardController = Get.find<DashboardController>();
+    _zoomDrawerController = ZoomDrawerController();
 
     _authController.listenToUserDataonDB(onGetUserData: (user) {
       _authController.setUser(user: user);
@@ -61,104 +65,122 @@ class _DashboardMainState extends State<DashboardMain> {
       'assets/images/profile.svg',
     ];
 
-    return Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Theme.of(context).primaryColor,
-            statusBarBrightness: Brightness.light,
-            systemNavigationBarColor: Theme.of(context).primaryColorDark,
-            systemNavigationBarIconBrightness: Brightness.dark),
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0,
-        leadingWidth: 88,
-        toolbarHeight: 70,
-        leading: UnconstrainedBox(
-          child: SvgPicture.asset(
-            'assets/images/logo.svg',
-            width: 35,
-            height: 35,
-          ),
-        ),
-        actions: [
-          //  profile picture
-          SizedBox(
-              width: 30,
-              height: 30,
-              child: Stack(
-                children: [
-                  const Align(
-                      alignment: Alignment.center,
-                      child: Avatar(avatarUrl: '', size: Size(30, 30))),
+    return ZoomDrawer(
+        controller: _zoomDrawerController,
+        style: DrawerStyle.defaultStyle,
+        menuBackgroundColor: Theme.of(context).primaryColor,
+        mainScreenScale: 0.2,
+        slideWidth: 250,
+        mainScreenTapClose: true,
+        showShadow: true,
+        borderRadius: 24,
+        menuScreen: const MenuScreen(),
+        mainScreen: Scaffold(
+          appBar: AppBar(
+            systemOverlayStyle: SystemUiOverlayStyle(
+                statusBarColor: Theme.of(context).primaryColor,
+                statusBarBrightness: Brightness.light,
+                systemNavigationBarColor: Theme.of(context).primaryColorDark,
+                systemNavigationBarIconBrightness: Brightness.dark),
+            backgroundColor: Theme.of(context).primaryColor,
+            elevation: 0,
+            leadingWidth: 88,
+            toolbarHeight: 70,
+            leading: UnconstrainedBox(
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
+                width: 35,
+                height: 35,
+              ),
+            ),
+            actions: [
+              //  profile picture
+              SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: Stack(
+                    children: [
+                      Align(
+                          alignment: Alignment.center,
+                          child: Obx(() => Avatar(
+                              avatarUrl:
+                                  _authController.user.value?.avatarUrl ?? '',
+                              size: const Size(30, 30)))),
 
-                  //  live status checker
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Obx(
-                      () => Container(
-                        width: 8,
-                        height: 8,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: _coreController.hasInternet.value
-                                ? Colors.greenAccent
-                                : Colors.redAccent),
-                      ),
-                    ),
-                  )
-                ],
-              )),
-          const SizedBox(width: 8),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search_rounded,
-                  color: Theme.of(context).primaryColorDark)),
-          IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                'assets/images/menu.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(
-                    Theme.of(context).primaryColorDark, BlendMode.srcIn),
-              )),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
-        surfaceTintColor: Colors.transparent,
-        height: 70,
-        padding: EdgeInsets.zero,
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-              color: Theme.of(context).primaryColorDark,
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(20), topLeft: Radius.circular(20))),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: bottomBarItemAssets
-                .map((asset) => Obx(
-                      () => BottomBarItem(
-                          asset: asset,
-                          isActive: _dashboardController.activeTabIndex.value ==
-                              bottomBarItemAssets.indexOf(asset),
-                          onTap: () {
-                            _dashboardController.setActiveTabIndex(
-                                index: bottomBarItemAssets.indexOf(asset));
-                          }),
-                    ))
-                .toList(),
+                      //  live status checker
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Obx(
+                          () => Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _coreController.hasInternet.value
+                                    ? Colors.greenAccent
+                                    : Colors.redAccent),
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+              const SizedBox(width: 8),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search_rounded,
+                      color: Theme.of(context).primaryColorDark)),
+              IconButton(
+                  onPressed: () {
+                    //  open zoom drawer
+                    _zoomDrawerController.toggle?.call();
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/images/menu.svg',
+                    width: 24,
+                    height: 24,
+                    colorFilter: ColorFilter.mode(
+                        Theme.of(context).primaryColorDark, BlendMode.srcIn),
+                  )),
+            ],
           ),
-        ),
-      ),
-      body: Obx(
-        () => IndexedStack(
-          index: _dashboardController.activeTabIndex.value,
-          children: screens,
-        ),
-      ),
-    );
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.transparent,
+            surfaceTintColor: Colors.transparent,
+            height: 70,
+            padding: EdgeInsets.zero,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColorDark,
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: bottomBarItemAssets
+                    .map((asset) => Obx(
+                          () => BottomBarItem(
+                              asset: asset,
+                              isActive:
+                                  _dashboardController.activeTabIndex.value ==
+                                      bottomBarItemAssets.indexOf(asset),
+                              onTap: () {
+                                _dashboardController.setActiveTabIndex(
+                                    index: bottomBarItemAssets.indexOf(asset));
+                              }),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ),
+          body: Obx(
+            () => IndexedStack(
+              index: _dashboardController.activeTabIndex.value,
+              children: screens,
+            ),
+          ),
+        ));
   }
 }

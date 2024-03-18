46c3f513606b22f66d976a7a01c8221769da1bfa
KenStarry <starrycodes@gmail.com>
2024-03-18 14:54:44 +0300

    Implemented profile page

--- lib/feature_profile/presentation/screens/profile_page.dart
+++ lib/feature_profile/presentation/screens/profile_page.dart
@@ -10,6 +10,30 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            const SliverToBoxAdapter(child: SizedBox(height: 24)),
+            //  title
+            SliverToBoxAdapter(
+                child: Text('Profile',
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.titleMedium!.fontSize,
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).textTheme.titleLarge!.color))),
+            const SliverToBoxAdapter(child: SizedBox(height: 24)),
+
+          ],
+        ),
+      ),
+    );
   }
 }
